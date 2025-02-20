563aa2ef0b22f7a4a7b770b4253bf9d69b1b9c97
machida-yosuke <machida-yosuke@kayac.com>
2025-02-20 12:43:23 +0900

    tailwind google font

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 export const App = ()=> {
   return (
-      <h1 className="text-3xl font-bold underline bg-shark-950">
+      <h1 className="text-3xl font-bold underline bg-shark-50">
         Hello world!
       </h1>
   );
